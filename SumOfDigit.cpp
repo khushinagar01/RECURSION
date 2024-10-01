fbefcf6fd29028d5bb5fd465cd6f244f7d980e80
KHUSHI NAGAR <155895880+khushinagar01@users.noreply.github.com>
2024-10-01 22:55:42 +0530

    Add files via upload

---
+++ SumOfDigit.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int sumOfDigit(int n)
+{  
+    //Base case :return 0 if the number is 0
+    if(n==0)
+    {
+        return 0;
+    }
+    //Recursive case : add the last digit to the sum of the rest of the digit
+    return (n%10 + sumOfDigit(n/10));
+}
+int main()
+{
+    int n;
+    //Enter the number from the user
+    cout<<"Enter the number:";
+    cin>>n;
+    //Call the recursive function and print the output
+    int SUM=sumOfDigit(n);
+    cout<<"Sum is:"<<SUM<<endl;
+    return 0;
+}
