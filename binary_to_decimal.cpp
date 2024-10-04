04883154ef6a675bd60ab2bcdb20ec1836ed9347
KHUSHI NAGAR <155895880+khushinagar01@users.noreply.github.com>
2024-10-04 21:26:22 +0530

    Add files via upload

---
+++ binary_to_decimal.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int binaryToDecimal(int binary,int base=1)
+{    
+    
+    //BASE CASE
+    if(binary==0)
+     return 0;
+    
+    //GET THE LAST DIGIT
+    int lastDigit=binary%10;
+   
+    //RECURSIVE CALL
+    return lastDigit*base + binaryToDecimal(binary/10,base*2);
+}
+int main()
+{
+    int binary;
+    cout<<"Enter the binary number:";
+    cin>>binary;
+    cout<<"Decimal equivalent: "<<binaryToDecimal(binary)<<endl;
+    return 0;
+}
